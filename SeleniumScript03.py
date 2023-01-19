f3d80c570b7d0e70f560d02eb7ec3cfdcb7c074c
Nathaniel Waldron <nathaniel@landistechnologies.com>
2023-01-19 13:58:28 -0500

    Script 03, ActionChains nested in for loop

--- SeleniumScript03.py
+++ SeleniumScript03.py
@@ -2,20 +2,52 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-import time
+from selenium.webdriver.common.action_chains import ActionChains
 
-def findLink(driver, linkText):
-    return WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.LINK_TEXT, linkText))
+def getCount(count):
+    try:
+        v = int(str(count).split(" ")[0])
+        return v
+    except:
+        return 0
 
 #strings
-#PATH = "C:\Program Files (x86)\chromedriver.exe" # deprecated
-url = "https://techwithtim.net"
+url = "https://orteil.dashnet.org/cookieclicker/"
+english = "langSelect-EN"
+bigCookie = "bigCookie"
+cookieCount = "cookies"
 
 options = Options()
 options.page_load_strategy = 'normal'
 driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
-
 driver.get(url)
 
-time.sleep(10)
+print("Looking for language button")
+language = driver.find_element(By.ID, english)
+language.click()
+
+for i in range(5000): 
+    try:
+        print("Cycle " + str(i))
+        cookie = driver.find_element(By.ID, bigCookie)
+        counter = driver.find_element(By.ID, cookieCount)
+        cookies = getCount(counter.text)
+        items = [driver.find_element(By.ID, "productPrice" + str(i)) for i in range(2, -1, -1) ]
+        
+        # I tried this both ways, and it's significantly faster to not use "ActionChains"
+        # actions = ActionChains(driver)
+        # actions.move_to_element(cookie)
+        # actions.click(cookie)
+        # actions.perform()
+        cookie.click()
+
+        for item in items:
+            price = getCount(item.text)
+            if price > 0 and price <= cookies:
+                actions = ActionChains(driver)
+                actions.move_to_element(item)
+                actions.click()
+                actions.perform()
+    except:
+        print(f"Cycle {i} Failed :-(")
