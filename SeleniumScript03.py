d6cff7c8512014c79acae66b8cefeff8f38562aa
Nathaniel Waldron <nathaniel@landistechnologies.com>
2023-01-19 09:23:59 -0500

    finished 02, starting 03

---
+++ SeleniumScript03.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+import time
+
+def findLink(driver, linkText):
+    return WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.LINK_TEXT, linkText))
+
+#strings
+#PATH = "C:\Program Files (x86)\chromedriver.exe" # deprecated
+url = "https://techwithtim.net"
+
+options = Options()
+options.page_load_strategy = 'normal'
+driver = webdriver.Chrome(options=options)
+driver.implicitly_wait(10)
+
+driver.get(url)
+
+time.sleep(10)
