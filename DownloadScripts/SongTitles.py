909ef09f26397ff2221adcfaf529fbc570358781
Nathaniel Waldron <nate@landistechnologies.com>
2023-04-21 14:25:15 -0400

    working on refactoring downloader to allow searching for multiple artists and downloading in sequence

---
+++ DownloadScripts/SongTitles.py
@@ -0,0 +1,63 @@
+def getSongTitles():
+    return """
+Less Like Me, Zach Williams
+Say I Won't, MercyMe
+Battle Belongs, Phil Wickham
+Fires, Jordan St. Cyr
+Good God Almighty, Crowder
+Next to Me, Jordan Feliz
+Help Is On The Way (Maybe Midnight), TobyMac
+Sparrows, Cory Asbury
+Every Step of the Way, Cade Thompson
+Amen, For King & Country
+Raise A Hallelujah, Bethel Music & Jonathan David Helser & Melissa Helser
+Nobody, Casting Crowns ft. Matthew West
+Rescue, Lauren Daigle
+God Only Knows, For King & Country
+Graves Into Gardens, Elevation Worship & Brandon Lake
+Out of My Hands, Jeremy Camp
+Rise Up (Lazarus), CAIN
+Truth Be Told, Matthew West
+New Today, Micah Tyler
+New Day, Danny Gokey
+Amen, For King & Country
+Say I Won't, MercyMe
+Faithful God, I Am They
+Image of God, We Are Messengers
+God of Revival, Brian Johnson & Jenn Johnson
+Confidence, Sanctus Real
+The Old Rugged Cross, Carrie Underwood
+There Will Come a Day, Faith Hill
+Jesus Take the Wheel, Carrie Underwood
+Thank God I Got Her, Jonny Diaz
+Fight for You, Grayson/Reed
+Fallin in Love, Britt Nicole
+God Gave Me You, Dave Barnes
+Made for Me, TobyMac
+Stand In Your Love, Josh Baldwin
+Evidence, Josh Baldwin
+10,000 Reasons (Bless The Lord), Matt Redman
+Oceans (Where Feet May Fail), Hillsong United
+Goodness Of God, Bethel Music & Jenn Johnson
+The Blessing, Cody Carnes & Elevation Worship & Kari Jobe
+Build My Life, Housefires
+How Great Is Our God, Chris Tomlin
+The Heart Of Worship, Matt Redman
+Hosanna, Hillsong Worship
+How Great Thou Art, Carrie Underwood
+Amazing Grace (My Chains Are Gone), Chris Tomlin
+Cry Out to Jesus, Third Day
+The Motions, Mathew West
+Come as You Are, Crowder
+Love Comes Down, Matt Maher
+God of Grace, Finding Faith
+You Say, Lauren Daigle
+You Are More, Tenth Avenue North
+Revelation Song, Kari Jobe
+This is Amazing Grace, Phil Wickham
+Open the Eyes of My Heart, Paul Baloche
+Cornerstone, Hillsong Worship
+Forever, Kari Jobe
+King of Kings, Hillsong Worship
+Jesus Paid It All, Kristian Stanfill / Passion
+"""
