ce00b816cea8c2fea4cc51a624b5d8669b472018
Nathaniel Waldron <nate@landistechnologies.com>
2023-04-21 15:05:57 -0400

    multi search deployed successfully

--- DownloadScripts/Downloader.py
+++ DownloadScripts/Downloader.py
@@ -5,9 +5,16 @@ from HtmlFile import getHtml
 from DriverBuilder import getDriver
 from Logger import log
 
-def inputVideoUrl(playlistUrlWithoutIndex, enter, driver, i):
+#declarations
+url = "https://ytmp3.nu/7/youtube-to-mp3"
+googleUrl = "https://www.google.com/search?q=hillsong%20site%3Ayoutube.com"
+videoUrlBase = "https://www.youtube.com/watch?v="
+directoryBase = "C:\\Users\\nathaniel\\Downloads\\Music"
+enter = u'\ue007'
+videos = []
+wait = 15
+def inputVideoUrl(enter, driver, videoUrl):
     urlInput = driver.find_element(By.ID, "url")
-    videoUrl = playlistUrlWithoutIndex + videos[i]
     urlInput.send_keys(videoUrl)
     urlInput.send_keys(enter)
 
@@ -45,35 +52,50 @@ def clickConvertNext(driver):
     log("Convert next button found, clicking convert next")
     convertNextButton.click()
 
-#declarations
-url = "https://ytmp3.nu/7/youtube-to-mp3"
-googleUrl = "https://www.google.com/search?q=hillsong%20site%3Ayoutube.com"
-videoUrlBase = "https://www.youtube.com/watch?v="
-directoryBase = "C:\\Users\\nathaniel\\Downloads\\Music"
-directory = ""
-enter = u'\ue007'
-html = ""
-videos = []
-wait = 15
+def runDownloadLoop(driver, directory, videos):
+    if not path.exists(directory):
+        log("Creating directory {}".format(directory))
+        makedirs(directory)
+        
+    i = len(listdir(directory))
+    for video in videos:
+        log("Cycle " + str(i))
+        #the video url is the playlist url without the index and the video id
+        videoUrl = videoUrlBase + video
+        log("Video url: " + videoUrl)
+        inputVideoUrl(enter, driver, videoUrl)
+        i += 1
+            #if the error button is found, click it and continue to the next loop
+        try:
+            ifErrorClickBack(driver)
+                #if cannot find download button, check for popup or error
+            for j in range(35):
+                try:
+                    log("Try {} to download".format(j + 1))
+                    clickDownload(driver)
+                    closeIfPopUp(driver)
+                    break
+                except:
+                    log("Could not find download button")
+                    closeIfPopUp(driver)
+            clickConvertNext(driver)
+        except:
+            continue
+    return i
 
-def setDirectory(artist):
+def getDirectory(artist):
     log("Setting directory to {}".format(artist))
     global directory
-    directory = directoryBase + "\\" + artist
+    return directoryBase + "\\" + artist
 
-#def set html
-def setHtml(input):
-    global html
-    html = input
-
-def download():
+def download(driver = None, html = "", directory = "C:\\Users\\nathaniel\\Downloads\\Music"):
     log("Getting driver")
     if driver == None:
         headless = input("Run headless? (y/n): ").lower() == "y"
         driver = getDriver(wait, directory, headless)
 
     
-    if html == "":
+    if html == None or html == "":
         html = getHtml()
     if html == "":
         # use the code in ScrapeLinks.py to get the video ids
@@ -83,7 +105,9 @@ def download():
             driver.get(playlistUrl)
             html = driver.page_source
         except:
-            html = input("That didn't work. Please paste in the html: ")
+            log("Error getting html from playlist url")
+            driver.quit()
+            exit()
 
     videos = getVideoIds(html)
 
@@ -99,31 +123,7 @@ def download():
     driver.get(url)
 
     try:
-        if not path.exists(directory):
-            log("Creating directory {}".format(directory))
-            makedirs(directory)
-        
-        i = len(listdir(directory))
-        for video in videos:
-            log("Cycle " + str(i))
-            inputVideoUrl(videoUrlBase, enter, driver, i)
-            i += 1
-            #if the error button is found, click it and continue to the next loop
-            try:
-                ifErrorClickBack(driver)
-                #if cannot find download button, check for popup or error
-                for j in range(35):
-                    try:
-                        log("Try {} to download".format(j + 1))
-                        clickDownload(driver)
-                        closeIfPopUp(driver)
-                        break
-                    except:
-                        log("Could not find download button")
-                        closeIfPopUp(driver)
-                clickConvertNext(driver)
-            except:
-                continue
+        i = runDownloadLoop(driver, directory, videos)
 
     except Exception as e:
         log("Error on cycle " + str(i))
@@ -132,6 +132,7 @@ def download():
         log("Closing driver")
         driver.quit()
         exit()
+
 
 if __name__ == "__main__":
     download()
--- DownloadScripts/MultiSearcher.py
+++ DownloadScripts/MultiSearcher.py
@@ -1,6 +1,6 @@
 from DriverBuilder import getDriver
 from Logger import log
-from Downloader import download, setDirectory, setHtml
+from Downloader import download, getDirectory
 
 #declarations
 googleUrl = [
@@ -16,25 +16,27 @@ wait = 15
 
 def run():
     log("Starting MultiSearcher")
-    #get the list of artists
+    
     for artist in artists:
         log("Searching for {}".format(artist))
-        #for each artist, navigate the driver to the googleUrl search page for that artist
+        
         driver = getDriver()
         url = googleUrl[0] + artist + googleUrl[1]
         log("Navigating to {}".format(url))
         driver.get(url)
-
-        #extract the html from the page
+        
         html = driver.page_source
         log("html line count: {}".format(len(html.splitlines())))
-        #in Downloader.py set the html to the html from the page
-        log("Setting html")
-        setHtml(html)
-        #set the directory to the artist name
-        log("Setting directory to {}".format(artist))
-        setDirectory(artist)
-        #call the download function\
-        log("Downloading")
-        download()
+        
+        directory = getDirectory(artist)
+        log("Setting directory to {}".format(directory))
+        
+        log("Starting download")
+        download(driver, html, directory)
+        
+    log("Complete.")
+    driver.close()
+    exit()
 
+if __name__ == "__main__":
+    run()
